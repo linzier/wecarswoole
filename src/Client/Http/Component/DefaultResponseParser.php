0e1f4a00878d90c600879063fda3dc57c4564e8d
songlin.zhang <songlin.zhang@weicheche.cn>
2022-04-22 19:01:06 +0800

    默认采用JsonResponseParser

--- src/Client/Http/Component/DefaultResponseParser.php
+++ src/Client/Http/Component/DefaultResponseParser.php
@@ -2,31 +2,12 @@
 
 namespace WecarSwoole\Client\Http\Component;
 
-use WecarSwoole\Client\Config\Config;
-use WecarSwoole\Client\Contract\IResponseParser;
-use WecarSwoole\Client\Response;
-
 /**
  * 默认的响应解析器
+ * 用 JsonResponseParser
  * Class DefaultResponseParser
  * @package WecarSwoole\Client\Http\Component
  */
-class DefaultResponseParser implements IResponseParser
+class DefaultResponseParser extends JsonResponseParser
 {
-    private $config;
-
-    public function __construct(Config $config)
-    {
-        $this->config = $config;
-    }
-
-    /**
-     * 默认实现：什么也不处理
-     * @param Response $response
-     * @return Response
-     */
-    public function parser(Response $response): Response
-    {
-        return $response;
-    }
 }
--- tests/test.php
+++ tests/test.php
@@ -4,14 +4,64 @@ use WecarSwoole\Client\Response;
 
 include_once './base.php';
 
-class A
-{
-    public static function say()
-    {
-        echo "hello say\n";
-    }
-}
-
-$c = new ReflectionClass(A::class);
-$c->getMethod('say')->invoke(null);
-
+$s = '{
+	"details": {
+		"0": {
+			"oil_amt": 11.28,
+			"order_amt": 8,
+			"money_amt": 80,
+			"money_wcc_amt": 80,
+			"contract_discnt_amt": 0,
+			"money_save_amt": 0
+		},
+		"92": {
+			"oil_amt": 1.34,
+			"order_amt": 1,
+			"money_amt": 10,
+			"money_wcc_amt": 10,
+			"contract_discnt_amt": 0,
+			"money_save_amt": 0
+		},
+		"CNG": {
+			"oil_amt": 2.65,
+			"order_amt": 1,
+			"money_amt": 10,
+			"money_wcc_amt": 10,
+			"contract_discnt_amt": 0,
+			"money_save_amt": 0
+		}
+	},
+	"退款": {
+		"0": {
+			"oil_amt": 11.28,
+			"order_amt": 8,
+			"money_amt": -80,
+			"money_wcc_amt": -80,
+			"contract_discnt_amt": 0
+		},
+		"92": {
+			"oil_amt": 1.34,
+			"order_amt": 1,
+			"money_amt": -10,
+			"money_wcc_amt": -10,
+			"contract_discnt_amt": 0
+		},
+		"CNG": {
+			"oil_amt": 2.65,
+			"order_amt": 1,
+			"money_amt": -10,
+			"money_wcc_amt": -10,
+			"contract_discnt_amt": 0
+		}
+	},
+	"summary": {
+		"order_amt": 20,
+		"oil_amt": 30.55,
+		"money_amt": 0,
+		"money_wcc_amt": 0,
+		"money_save_amt": 0,
+		"contract_discnt_amt": 0
+	}
+}';
+$a = json_decode($s, true, 30, ~JSON_NUMERIC_CHECK);
+var_export($a);
