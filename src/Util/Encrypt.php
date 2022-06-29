752aa6f337fae47ca5dea29f57b5bb92245d6172
songlin.zhang <songlin.zhang@weicheche.cn>
2022-06-29 16:49:19 +0800

    f

--- src/Util/Encrypt.php
+++ src/Util/Encrypt.php
@@ -6,7 +6,9 @@ class Encrypt
 {
     public static function enc(string $txt, string $secret): string
     {
-        $enc = openssl_encrypt($txt, "AES-128-CBC", $secret, OPENSSL_RAW_DATA);
+        $len = openssl_cipher_iv_length("AES-128-CBC");
+        $iv = str_pad(substr($secret, 0, $len), $len, '0');
+        $enc = openssl_encrypt($txt, "AES-128-CBC", $secret, OPENSSL_RAW_DATA, $iv);
         if (!$enc) {
             return '';
         }
@@ -16,7 +18,9 @@ class Encrypt
 
     public static function dec(string $txt, string $secret): string
     {
-        $dec = openssl_decrypt(base64_decode($txt), "AES-128-CBC", $secret, OPENSSL_RAW_DATA);
+        $len = openssl_cipher_iv_length("AES-128-CBC");
+        $iv = str_pad(substr($secret, 0, $len), $len, '0');
+        $dec = openssl_decrypt(base64_decode($txt), "AES-128-CBC", $secret, OPENSSL_RAW_DATA, $iv);
         if (!$dec) {
             return '';
         }
