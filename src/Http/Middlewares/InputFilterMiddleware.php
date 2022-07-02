d6a00db0596001d77417127fbbbde2be1e19f1fe
songlin.zhang <songlin.zhang@weicheche.cn>
2022-07-02 10:26:20 +0800

    更换xss处理库

--- src/Http/Middlewares/InputFilterMiddleware.php
+++ src/Http/Middlewares/InputFilterMiddleware.php
@@ -5,8 +5,7 @@
  use EasySwoole\EasySwoole\Config;
  use EasySwoole\Http\Request;
  use EasySwoole\Http\Response;
- use HTMLPurifier;
- use HTMLPurifier_Config;
+ use voku\helper\AntiXSS;
  use WecarSwoole\Http\Controller;
  use WecarSwoole\Middleware\Middleware;
  use WecarSwoole\Middleware\Next;
@@ -25,17 +24,22 @@ class InputFilterMiddleware extends Middleware implements IControllerMiddleware
 
     public function before(Next $next, Request $request, Response $response)
     {
+        static $antiXss;
+
+        if (!$antiXss) {
+            $antiXss = new AntiXSS();
+            $antiXss->removeEvilAttributes(['style']);
+        }
+
         $xssFilter = boolval(intval(Config::getInstance()->getConf('xss_filter') ?? 0));
         $trimSpace = boolval(intval(Config::getInstance()->getConf('trim_whitespace') ?? 1));
 
-        $purifier = new HTMLPurifier(HTMLPurifier_Config::createDefault());
-
-        $this->proxy->requestParams = $this->filter($this->proxy->requestParams, $xssFilter, $trimSpace, $purifier);
+        $this->proxy->requestParams = $this->filter($this->proxy->requestParams, $xssFilter, $trimSpace, $antiXss);
 
         return $next($request, $response);
     }
 
-    private function filter($input, bool $xssFilter, bool $trimSpace, HTMLPurifier $purifier)
+    private function filter($input, bool $xssFilter, bool $trimSpace, AntiXSS $antiXss)
     {
         if (!$xssFilter && !$trimSpace || !$input || !is_string($input) && !is_array($input)) {
             return $input;
@@ -43,26 +47,26 @@ class InputFilterMiddleware extends Middleware implements IControllerMiddleware
 
         // 字符串
         if (is_string($input)) {
-            return $this->innerFilter($input, $xssFilter, $trimSpace, $purifier);
+            return $this->innerFilter($input, $xssFilter, $trimSpace, $antiXss);
         }
 
         // 数组
         $out = [];
         foreach ($input as $k => $v) {
-            $out[$k] = $this->filter($v, $xssFilter, $trimSpace, $purifier);
+            $out[$k] = $this->filter($v, $xssFilter, $trimSpace, $antiXss);
         }
 
         return $out;
     }
 
-    private function innerFilter($input, bool $xssFilter, bool $trimSpace, HTMLPurifier $purifier)
+    private function innerFilter($input, bool $xssFilter, bool $trimSpace, AntiXSS $antiXss)
     {
         if ($trimSpace) {
             $input = trim($input);
         }
 
         if ($xssFilter) {
-            $input = $purifier->purify($input);
+            $input = html_entity_decode($antiXss->xss_clean($input));
         }
 
         return $input;
--- tests/test.php
+++ tests/test.php
@@ -2,17 +2,13 @@
 
 include_once './base.php';
 
-use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Lcobucci\JWT\Signer\Key\InMemory;
-use Lcobucci\JWT\UnencryptedToken;
-use Lcobucci\JWT\Validation\RequiredConstraintsViolated;
-use Lcobucci\JWT\Validation\Constraint\SignedWith;
-use Lcobucci\JWT\Validation\Constraint\IssuedBy;
-use Lcobucci\JWT\Validation\Constraint\StrictValidAt;
+use voku\helper\AntiXSS;
 
-$dirty_html = '<div><img onload="alert(\'hello\')" src="http://dd.com" onclick="document.window.reload()" /><script>alert("dd")</script></div>';
-$config = HTMLPurifier_Config::createDefault();
-$purifier = new HTMLPurifier($config);
-$clean_html = $purifier->purify($dirty_html);
-echo $clean_html;
+$antiXss = new AntiXSS();
+$antiXss->removeEvilAttributes(array('style'));
+
+$t = '<li style="list-style-image: url(javascript:alert(0))">ddd</li> <>';
+
+$s = $antiXss->xss_clean($t);
+
+echo $s,"=======: ",html_entity_decode($s), "\n";
