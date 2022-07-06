e9552f4619db78415e58016a8b97ad245f8ca850
songlin.zhang <songlin.zhang@weicheche.cn>
2022-07-06 14:36:24 +0800

    f

---
+++ src/Client/Http/Component/RawResponseParser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace WecarSwoole\Client\Http\Component;
+
+use Psr\Http\Message\ResponseInterface;
+use WecarSwoole\Client\Config\Config;
+use WecarSwoole\Client\Contract\IResponseParser;
+use WecarSwoole\Client\Response;
+use WecarSwoole\Client\Response\RawResponse;
+
+class RawResponseParser implements IResponseParser
+{
+    private $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    public function parser(string $url, ResponseInterface $response, bool $isRealRequest): Response
+    {
+        return new RawResponse(
+            $response->getBody()->read($response->getBody()->getSize()),
+            $response->getStatusCode(),
+            $response->getReasonPhrase(),
+            $isRealRequest,
+            $url
+        );
+    }
+}
