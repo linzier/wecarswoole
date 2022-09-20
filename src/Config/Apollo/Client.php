97f12f18872c8596abe58a0e5c4bb9c5f6996e3e
songlin.zhang <linvanda@linzideMacBook-Pro.local>
2022-09-20 15:28:04 +0800

    apollo请求参数增加兼容

--- src/Config/Apollo/Client.php
+++ src/Config/Apollo/Client.php
@@ -229,6 +229,7 @@ class Client
         $params = [
             'appId' => $this->appId,
             'cluster' => $this->cluster,
+            'clusterName' => $this->cluster,
             'notifications' => json_encode(array_values($this->notifications)),
         ];
 
